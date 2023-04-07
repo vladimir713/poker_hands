cec1190f32dd0081aac2982ae2bcb23f78a8c3c9
vladimir713@mail.ru <vladimir713@mail.ru>
2023-04-07 14:26:30 +0300

    Осталость написать компаратор

--- src/main/java/PokerHand.java
+++ src/main/java/PokerHand.java
@@ -1,7 +1,16 @@
-public class PokerHand {
+public class PokerHand implements Comparable<PokerHand>{
     private String hand;
 
     public PokerHand(String hand) {
         this.hand = hand;
+    }
+
+    public String getHand() {
+        return hand;
+    }
+
+    @Override
+    public int compareTo(PokerHand o) {
+        return 0;
     }
 }
